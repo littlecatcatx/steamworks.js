fda3f2056c1ce2efc97739d4a593f618c8477d8a
zjx20 <zhoujianxiong2@gmail.com>
2025-07-21 23:59:02 +0800

    make logger.rs more general

--- src/logger.rs
+++ src/logger.rs
@@ -1,15 +1,29 @@
 
-use std::fs::OpenOptions;
+use std::env;
+use std::fs::{self, OpenOptions};
 use std::io::Write;
+use std::sync::Once;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+static LOG_INIT: Once = Once::new();
+
 pub fn log_to_file(message: &str) {
-    let path = "/tmp/my_streamworks.log";
+    let mut path = env::temp_dir();
+    path.push("my_streamworks.log");
+
+    LOG_INIT.call_once(|| {
+        // This block runs only once per process
+        // Attempt to remove the old log file, ignoring errors if it doesn't exist
+        let _ = fs::remove_file(&path);
+        // Print the log path to the console for easy access
+        println!("[steamworks.js] logging to: {}", path.display());
+    });
+
     let mut file = OpenOptions::new()
         .create(true)
         .write(true)
         .append(true)
-        .open(path)
+        .open(&path)
         .unwrap();
 
     let now = SystemTime::now();
